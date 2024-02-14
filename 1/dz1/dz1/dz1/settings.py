5798f1bbd61b649597e72a3b3dc3dc61a704ec55
Anton <anton6733@gmail.com>
2024-02-14 07:30:25 +0200

    logs

--- 1/dz1/dz1/dz1/settings.py
+++ 1/dz1/dz1/dz1/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import pdb
 from pathlib import Path
 
+import logging
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = 'django-insecure-s#@-w2nzjg%8=59tim^7_@p1*t$*xo&6!rn6(nm_nm99obx0lh
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dz1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -113,7 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -124,3 +120,40 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+logger = logging.Logger(__name__)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'main': {
+            'format': "{levelname}::{asctime}::{message}",
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'main', },
+        'file_main': {
+            'class': 'logging.FileHandler',
+            'formatter': 'main',
+            'filename': 'dz.log',
+            'encoding': 'UTF-8'
+        },
+    },
+    'loggers': {
+        'dz1': {
+            'handlers': ['console', 'file_main'],
+            'level': 'WARNING',
+        },
+        'lorem100': {
+            'handlers': ['console', 'file_main'],
+            'level': 'INFO',
+        },
+        'serve_vid': {
+            'handlers': ['console', 'file_main'],
+            'level': 'INFO',
+        },
+    }
+}
--- 1/dz1/dz1/serve_vid/views.py
+++ 1/dz1/dz1/serve_vid/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from typing import Callable
 
+from django.shortcuts import render, HttpResponse
+from logging import getLogger
 
 # Create your views here.
 
+logger = getLogger(__name__)
+
+
+def history(func: Callable):
+    def wrapper(*args, **kwargs):
+        nonlocal func
+        out = func(*args, **kwargs)
+        logger.info(f"{func.__name__}")
+        return out
+
+    return wrapper
+
+
+@history
 def get_yt(request):
     return HttpResponse(
         '<div style="display:flex;justify-content: center;"><iframe width="560" height="315" src="https://www.youtube.com/embed/Uyft9x-UP40?si=ogiR16k3kcECUS6R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>')
