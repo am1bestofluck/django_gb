726d651f4bb2369c8a4b5629be27ec9146752605
Anton <anton6733@gmail.com>
2024-02-19 08:48:03 +0200

    sem3..takes way more then expected. Fun tho

--- 2/sem2/blog_project/blog_models/views.py
+++ 2/sem2/blog_project/blog_models/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import pdb
+from typing import Union, List
+from django.db.models import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+from .models import Author, Article, Comment
+
 
 # Create your views here.
+
+class GetArticleHeaders(TemplateView):
+    # проверять будем на авторе 5, там есть публичные и приватные
+    template_name = "blog_models/all_headers.html"
+
+    def get_context_data(self, **kwargs):
+        aid = kwargs['author_id']
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Get all article titles of {Author.objects.filter(id=aid).first().full_name}'
+        context['articles'] = Article.objects.filter(author_id__exact=aid)
+        # pdb.set_trace()
+        return context
+
+
+def get_article_with_views_increment(request, **kwargs):
+    aid = kwargs['article_id']
+    article = get_object_or_404(Article, pk=aid)
+    author = Author.objects.filter(id=article.author.pk).first()
+    context = {"title": f"Get info on '{article.title}' of {author.full_name}"}
+    context.update({"details": article})
+    article.views += 1
+    article.save()
+    # pdb.set_trace()
+    return render(request, "blog_models/all_articles.html", {"context": context})
+
+
+class GetAllCommentsOnArticle(TemplateView):
+    template_name = "blog_models/all_comments.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pdb.set_trace()
+        art_id = kwargs['article_id']
+        article = get_object_or_404(Article,pk=art_id)
+        context['title']: str = f"Comments on '{article.title}'"
+        context['details'] = dict()
+        context['details']['title']: str = article.title
+        context['details']['content']: str = article.content
+        context['comment']: QuerySet[Comment] = Comment.objects.filter(article_id__exact=art_id)
+
+        pdb.set_trace()
+        return context
--- 2/sem2/blog_project/blog_project/settings.py
+++ 2/sem2/blog_project/blog_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,9 +25,9 @@ SECRET_KEY = 'django-insecure-)(6g#75k*#sw9ntly5=lzfrj$h$zh%)wgnod=(4lhd=k+n4vfq
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.0.9'
+    '127.0.0.1',
+    '192.168.0.9',
 ]
-
 
 # Application definition
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -114,12 +110,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static", ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
