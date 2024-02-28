404ccf81e94d5ab68ea08078279400008f7c06ee
Anton <anton6733@gmail.com>
2024-02-28 21:58:06 +0200

    ready

--- 2/sem2/blog_project/blog_models/admin.py
+++ 2/sem2/blog_project/blog_models/admin.py
@@ -1,3 +1,5 @@
+import pdb
+
 from django.contrib import admin
 from .models import Author, Comment, Article
 
@@ -6,14 +8,28 @@ from .models import Author, Comment, Article
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    # pdb.set_trace()
+    list_display = [i.name for i in Author._meta.fields]
+    list_display.remove('bio')
+    list_display_links = ['email']
+    readonly_fields = ['full_name']
+    fieldsets = (('for full_name', {'fields': ['full_name'], 'classes': ['collapse'],
+                                    'description': "That's hot and all, BUT WHERE DO I READ DOCS?"}),)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = [i.name for i in Comment._meta.fields]
+    list_display.remove('content')
+    readonly_fields = ['author', 'article']
+    fieldsets = (
+        ('descriptors', {'fields': ['author', 'article'], }),
+        ('text', {'fields': ['content'], 'classes': ['collapse']}))
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = [i.name for i in Article._meta.fields]
+    list_display.remove('content')
+    readonly_fields = ['id', 'author']
+    fieldsets = (('support', {'fields': ['id', 'author']}), ('content', {'fields': ['content']}),)
