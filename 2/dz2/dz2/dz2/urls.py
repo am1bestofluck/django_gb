92f4d74a1c7bd83f06df7e10ea1b7e823abb9492
Anton <anton6733@gmail.com>
2024-02-29 07:02:49 +0200

    ready

--- 2/dz2/dz2/dz2/urls.py
+++ 2/dz2/dz2/dz2/urls.py
@@ -18,6 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.utils.translation import gettext_lazy
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.forms import AuthenticationForm, forms
+
+secured_af = {"login": "ILook", "pw": "IntoYourEyes"}
+
+AdminSite.site_header = gettext_lazy(f'''login:'{secured_af["login"]}' pw:"{secured_af["pw"]}"''')
+AuthenticationForm.declared_fields['username'].initial = secured_af["login"]
+AuthenticationForm.declared_fields['password'].widget = forms.PasswordInput(
+    attrs={"autocomplete": "current-password", "value": secured_af["pw"]})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- 2/dz2/dz2/models/admin.py
+++ 2/dz2/dz2/models/admin.py
@@ -1,3 +1,48 @@
+import pdb
+
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Client, Order, Ware
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = [i.name for i in Client._meta.fields]
+    list_display_links = ['name']
+    readonly_fields = ['id', 'date_reg']
+    # pdb.set_trace()
+    fieldsets = (('personal', {'fields': ['name', 'mail', 'phone', 'path'],
+                               'description': 'stick to read-only, unless ASSURED'}),
+                 ('support', {'fields': readonly_fields, 'classes': ['collapse'],
+                              'description': 'why bother ;]'}))
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [i.name for i in Order._meta.fields]
+    list_display_links = ['client']
+    readonly_fields = ['id']
+    fieldsets = (('basic', {'fields': ['id', 'client', 'date_locked']}),
+                 ('positions', {'fields': ['wares','total'],
+                                'classes': ['collapse'],
+                                'description': 'джанго против ManyToManyField, очевидно =\\;'
+                                               'была бы неделька в запасе - может и написал бы обход.'
+                                               'Вроде как есть методичка в документации;'
+                                               'А так - имеем то что имеем'
+                                }))
+
+
+@admin.register(Ware)
+class WareAdmin(admin.ModelAdmin):
+    list_display = [i.name for i in Ware._meta.fields]
+    list_display_links = ['title']
+
+    readonly_fields = ['id', 'date_income']
+
+    fieldsets = (
+        ('Basic', {'fields': ['title', 'notes', 'price', 'quantity', 'date_income']}),
+        ('preview', {'fields': ['front_view'], 'classes': ['collapse in'],
+                     'description': 'На самом деле идея была в том чтобы вывести превью; но тут надо кроить шаблон...'
+                                    ' наверно не успеваю =\\'}))
