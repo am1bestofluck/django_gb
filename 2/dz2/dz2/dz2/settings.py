4ff60dd3aef45a86f4d60b51b597fc34de08e7c1
Anton <anton6733@gmail.com>
2024-03-03 07:00:45 +0200

    dotenv fixed ffs

--- 2/dz2/dz2/dz2/settings.py
+++ 2/dz2/dz2/dz2/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+import pdb
 from pathlib import Path
-
+from dotenv import  load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -22,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(zykqx+k$0#%bvog_bp$szh$swai9=s0mjv150b-752fz@bf(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+pdb.set_trace()
+DEBUG = False if os.getenv("DEBUG") == "False" else True
 
 ALLOWED_HOSTS = ["127.0.0.1",
-                 "192.168.0.9", ]
+                 "192.168.0.9",
+                 'localhost']
 
 # Application definition
 
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'dz2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_store.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DBNAME"),# ЭТО вабщеск тотал!
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': os.getenv("MYSQL_PORT"),
     }
 }
 
