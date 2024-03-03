9f0b2605409623e8cc8d67e841809c9718aaa8ac
Anton <anton6733@gmail.com>
2024-03-03 07:05:25 +0200

    dotenv for deploy

--- 2/dz2/dz2/dz2/settings.py
+++ 2/dz2/dz2/dz2/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': os.getenv("MYSQL_USER"),
         'PASSWORD': os.getenv("MYSQL_PASSWORD"),
         'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT"),
+        # 'PORT': os.getenv("MYSQL_PORT"),
     }
 }
 
